54e4c5bac242ae60374639ecd0f7ef55c826064c
DaMoo <git@damoo.zone>
2019-03-21 11:52:36 -0400

    Refactor to avoid unnecessary array creation

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,6 +2,6 @@ class Hamming
   def self.compute(strand1, strand2)
     raise ArgumentError, 'Strands must be of equal length' unless strand1.length == strand2.length
 
-    strand1.chars.zip(strand2.chars).count { |tuple| tuple[0] != tuple[1] }
+    strand1.each_char.with_index.count { |char, index| char != strand2[index] }
   end
 end
