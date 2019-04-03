16dba6283df6fc3367fd21c4e07e6ecaf087eeb2
DaMoo <git@damoo.zone>
2019-04-03 11:39:49 -0400

    Refactor and simplify code

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,8 +1,6 @@
-require 'set'
-
 class Isogram
   def self.isogram?(input)
-    sanitized_input = input.downcase.each_char.reject { |char| char =~ /\W/ }
-    sanitized_input.length == sanitized_input.to_set.length
+    sanitized_input = input.downcase.scan(/\w/)
+    sanitized_input.length == sanitized_input.uniq.length
   end
 end
